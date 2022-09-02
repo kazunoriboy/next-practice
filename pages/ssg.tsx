dec31dacb3f954eacd0fc7935922d75a05e90d12
kazunoriboy <matunorigmnw@gmail.com>
2022-09-02 23:44:55 +0900

    SSGの実装
    getStaticPropsにpropを記載すると、ビルド時に取得したものを埋め込んでページを生成できる

---
+++ pages/ssg.tsx
@@ -0,0 +1,38 @@
+import { NextPage, GetStaticProps, NextPageContext } from 'next'
+import Head from 'next/head'
+
+type SSGProps = {
+  message: string
+}
+
+const SSG: NextPage<SSGProps> = (props) => {
+  const { message } = props
+
+  return (
+    <div>
+      <Head>
+        <title>Static Site Generation</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <p>
+          このページは静的サイト生成によってビルド時に生成されたページです
+        </p>
+        <p>{message}</p>
+      </main>
+    </div>
+  )
+}
+
+export const getStaticProps: GetStaticProps<SSGProps> = async (context) => {
+  const timestamp = new Date().toLocaleString()
+  const message = `${timestamp}にgetStaticPropsが実行されました`
+  console.log(message, context)
+  return {
+    props: {
+      message,
+    }
+  }
+}
+
+export default SSG
