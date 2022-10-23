5e1a8f593e5384dabfae60552dba057d9bfab232
kazunoriboy <matunorigmnw@gmail.com>
2022-10-23 14:56:29 +0900

    継承を行う時は、styled関数の引数に親となるコンポーネントを渡してあげる

---
+++ pages/extends.tsx
@@ -0,0 +1,24 @@
+import { NextPage } from "next";
+import styled from "styled-components";
+
+const Text = styled.p`
+  color: bule;
+  font-weight: bold;
+`
+
+const BorderedText = styled(Text)`
+  padding: 8px 16px;
+  border: 3px solid blue;
+  border-radius: 8px;
+`
+
+const Page: NextPage = () => {
+  return (
+    <div>
+      <Text>Hello</Text>
+      <BorderedText>World</BorderedText>
+    </div>
+  )
+}
+
+export default Page
