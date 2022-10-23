1caa8caad53961c1c170d2b074db65f7f89b1b74
kazunoriboy <matunorigmnw@gmail.com>
2022-10-23 15:20:36 +0900

    Next.jsのコンポーネントにスタイルを使用する

--- pages/sample.tsx
+++ pages/sample.tsx
@@ -1,5 +1,32 @@
-function Sample() {
-  return <span>サンプルのページです</span>
+import { NextPage } from "next";
+import Link, { LinkProps } from "next/link";
+import styled from "styled-components";
+
+type BaseLinkProps = React.PropsWithChildren<LinkProps> & {
+  className?: string
+  children: React.ReactNode
 }
 
-export default Sample
+const BaseLink = (props: BaseLinkProps) => {
+  const { className, children, ...rest } = props
+  return (
+    <Link {...rest}>
+      <a className={className}>{children}</a>
+    </Link>
+  )
+}
+
+const StyledLink = styled(BaseLink)`
+  color: #1e90ff;
+  font-size: 2em;
+`
+
+const Page: NextPage = () => {
+  return (
+    <div>
+      <StyledLink href="/">Go to Index</StyledLink>
+    </div>
+  )
+}
+
+export default Page
