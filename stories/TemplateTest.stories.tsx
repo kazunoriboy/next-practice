3a81e3ac7e59a7a47baab29b840e4ee7a67caec1
kazunoriboy <matunorigmnw@gmail.com>
2022-10-31 21:00:30 +0900

    StyledComponentのpropsとchildrenをStorybookのControlsタブから変更できるようにする

---
+++ stories/TemplateTest.stories.tsx
@@ -0,0 +1,25 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { StyledButton } from "../pages/components/StyledButton";
+
+export default {
+  title: 'StyledButton',
+  component: StyledButton,
+  argTypes: {
+    variant: {
+      control: { type: 'radio' },
+      options: ['primary', 'success', 'transparent'],
+    },
+    children: {
+      control: { type: 'text' },
+    },
+  },
+} as ComponentMeta<typeof StyledButton>
+
+const Template: ComponentStory<typeof StyledButton> = (args) => <StyledButton {...args} />
+
+export const TemplateTest = Template.bind({})
+
+TemplateTest.args = {
+  variant: 'primary',
+  children: 'Primary'
+}
