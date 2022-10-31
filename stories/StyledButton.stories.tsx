afc64d841b55193bac66617a5b95c475d98ea916
kazunoriboy <matunorigmnw@gmail.com>
2022-10-31 20:50:40 +0900

    addon-actionsを使ってアクションを追加

--- stories/StyledButton.stories.tsx
+++ stories/StyledButton.stories.tsx
@@ -1,5 +1,7 @@
+import React, { useState } from "react";
 import { ComponentMeta } from "@storybook/react";
 import { StyledButton } from "../pages/components/StyledButton";
+import { action } from "@storybook/addon-actions"
 
 export default {
   title: 'StyledButton',
@@ -7,10 +9,17 @@ export default {
   argTypes: { onClick: { action: 'clicked' } },
 } as ComponentMeta<typeof StyledButton>
 
+const incrementAction = action('increment')
+
 export const Primary = (props: []) => {
+  const [count, setCount] = useState(0)
+  const onClick = (e: React.MouseEvent) => {
+    incrementAction(e, count)
+    setCount((c) => c + 1)
+  }
   return (
-    <StyledButton {...props} variant="primary">
-      Primary
+    <StyledButton {...props} variant="primary" onClick={onClick}>
+      Count: {count}
     </StyledButton>
   )
 }
