f99fdfb56b4ae806732ab3b84f9e83ed124fea4e
kazunoriboy <matunorigmnw@gmail.com>
2022-11-20 16:51:16 +0900

    テキスト入力を開始すると、spanの表示文字列が変わるかどうかのテスト

--- pages/components/DelayInput/index.spec.tsx
+++ pages/components/DelayInput/index.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen, RenderResult } from "@testing-library/react"
+import { render, screen, RenderResult, fireEvent } from "@testing-library/react"
 import { DelayInput } from "./index"
 
 describe('DelayInput', () => {
@@ -19,5 +19,17 @@ describe('DelayInput', () => {
     const spanNode = screen.getByTestId('display-text') as HTMLSpanElement
 
     expect(spanNode.textContent).toBe('入力したテキスト: ')
+  })
+
+  it('should display 「入力中...」 immediately after onChange event occurs', () => {
+    const inputText = 'Test Input Text'
+    const inputNode = screen.getByTestId('input-text') as HTMLInputElement
+
+    fireEvent.change(inputNode, { target: { value: inputText } })
+
+    const spanNode = screen.getByTestId('display-text') as HTMLSpanElement
+
+    expect(spanNode.textContent).toBe('入力中...')
+
   })
 })
