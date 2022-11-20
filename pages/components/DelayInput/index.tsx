35c0aaee1ac6cdd77fbd93c0ce15a634c8f9f26d
kazunoriboy <matunorigmnw@gmail.com>
2022-11-20 16:27:22 +0900

    非同期で入力内容を表示するインプットを作成

---
+++ pages/components/DelayInput/index.tsx
@@ -0,0 +1,48 @@
+import React, { useState, useCallback, useRef } from 'react'
+
+type DelayButtonProps = {
+  onChange: React.ChangeEventHandler<HTMLInputElement>
+}
+
+export const DelayInput = (props: DelayButtonProps) => {
+  const { onChange } = props
+
+  const [isTyping, setIsTyping] = useState(false)
+
+  const [inputValue, setInputValue] = useState('')
+
+  const [viewValue, setViewValue] = useState('')
+
+  const timerRef = useRef<ReturnType<typeof setTimeout> | null>(null)
+
+  const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setIsTyping(true)
+
+    setInputValue(e.target.value)
+
+
+    if (timerRef.current !== null) {
+      clearTimeout(timerRef.current)
+    }
+
+    timerRef.current = setTimeout(() => {
+      timerRef.current = null
+  
+      setIsTyping(false)
+  
+      setViewValue(e.target.value)
+  
+      onChange(e)
+    }, 1000)
+
+  }, [onChange])
+
+  const text = isTyping ? '入力中...' : `入力したテキスト: ${viewValue}`
+
+  return (
+    <div>
+      <input data-testid="input-text" value={inputValue} onChange={handleChange} />
+      <span data-testid="display-text">{text}</span>
+    </div>
+  )
+}
