71b1e69b727bfc3174177e30ff140b6e8c3d452b
kazunoriboy <matunorigmnw@gmail.com>
2022-11-20 17:06:02 +0900

    非同期関数のテスト
    useFakeTimersを使うことで、タイマー処理のエミュレートができる

--- pages/components/DelayInput/index.spec.tsx
+++ pages/components/DelayInput/index.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen, RenderResult, fireEvent } from "@testing-library/react"
+import { render, screen, RenderResult, fireEvent, act } from "@testing-library/react"
 import { DelayInput } from "./index"
 
 describe('DelayInput', () => {
@@ -6,6 +6,8 @@ describe('DelayInput', () => {
   let handleChange: jest.Mock
 
   beforeEach(() => {
+    jest.useFakeTimers()
+
     handleChange = jest.fn()
 
     renderResult = render(<DelayInput onChange={handleChange} />)
@@ -13,6 +15,8 @@ describe('DelayInput', () => {
 
   afterEach(() => {
     renderResult.unmount()
+
+    jest.useFakeTimers()
   })
 
   it('should display empty in span on initial render', () => {
@@ -32,4 +36,20 @@ describe('DelayInput', () => {
     expect(spanNode.textContent).toBe('入力中...')
 
   })
+
+  it('should display input text 1second agter onChange event occurs', async () => {
+    const inputText = 'Test Input Text'
+    const inputNode = screen.getByTestId('input-text') as HTMLInputElement
+
+    fireEvent.change(inputNode, { target: { value: inputText } })
+
+    await act(() => {
+      jest.runAllTimers()
+    })
+
+    const spanNode = screen.getByTestId('display-text') as HTMLSpanElement
+
+    expect(spanNode.textContent).toBe(`入力したテキスト: ${inputText}`)
+  })
+
 })
