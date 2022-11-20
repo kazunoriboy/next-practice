ae966c83351a9831fbb3b980345eb676f5c8f3ef
kazunoriboy <matunorigmnw@gmail.com>
2022-11-20 15:33:44 +0900

    jestの実行に必要なライブラリをインストール

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,20 @@
+// jest.config.js
 const nextJest = require('next/jest')
 
-const createJestConfig = nextJest({ dir: './' })
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+})
 
+// Add any custom config to be passed to Jest
+/** @type {import('jest').Config} */
 const customJestConfig = {
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  testEnvironment: 'jsdom',
+  // Add more setup options before each test is run
+  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
 }
 
-module.exports - createJestConfig(customJestConfig)
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig)
